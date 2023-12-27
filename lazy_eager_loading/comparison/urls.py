49337f8e2bcf4f468889e547781c783701969f2b
komarite <ulasgulhan9@gmail.com>
2023-12-27 12:05:53 +0300

    With raw sql

--- lazy_eager_loading/comparison/urls.py
+++ lazy_eager_loading/comparison/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('lazy/<int:id>', views.index_lazy, name='lazy'),
     path('lazy', views.index_lazy_all, name='lazy_all'),
     path('eager/<int:id>', views.index_eager, name='eager'),
-    path('eager', views.index_eager_all, name='eager_all')
+    path('eager', views.index_eager_all, name='eager_all'),
+    path('sql', views.index_eager_sql, name='eager_sql')
 ]
--- lazy_eager_loading/comparison/views.py
+++ lazy_eager_loading/comparison/views.py
@@ -3,6 +3,7 @@ import time
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Categories, Products, Suppliers, OrderDetails
+from django.db import connection
 
 # Create your views here.
 def index_lazy(request, id):
@@ -64,4 +65,28 @@ def index_eager_all(request):
     print(f'{start_time} - {end_time}')
     print(type(products))
     return render (request, 'eager.html', {'products': products})
+
+
+def index_eager_sql(request):
+    total_time = 0
+    num_queries = 10
+    for _ in range(num_queries):
+        start_time = time.time()
+        query = """
+            select p.ProductName, s.CompanyName from Products as p
+            inner join Suppliers as s on p.SupplierID = s.SupplierID
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            products = cursor.fetchall()
+            print(products)
+
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        total_time += elapsed_time
+
+    average_time = total_time / num_queries
+    print(f"Eager Loading with Raw SQL Average Time: {average_time} seconds\nEager Loading with Raw SQL Elapsed Time: {elapsed_time} seconds")
+
+    return render(request, 'eager_sql.html', {'products': products})
 
